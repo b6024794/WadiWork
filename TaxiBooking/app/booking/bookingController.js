d3a22ae62396a52932b55aec0f3d67639e8478a4
b6024794 <33861818+b6024794@users.noreply.github.com>
2018-03-20 14:46:10 +0000

    Add files via upload

--- TaxiBooking/app/booking/bookingController.js
+++ TaxiBooking/app/booking/bookingController.js
@@ -1,4 +1,6 @@
 ﻿angular.module("bookingApp").controller("bookingController", function ($scope, $http) {
+    var bookingId;
+
     $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking")
         .success(function (response) {
             $scope.bookings = response;
@@ -6,10 +8,24 @@
         .error(function (error) {
             $scope.errorMessage = error;
         });
+    $scope.init = function () {
+        $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking")
+            .success(function (response) {
+                $scope.bookings = response;
+            })
 
+    };
+  
+
+
+
+    //A function to add details to the booking service  
     $scope.addBooking = function () {
+
+
+
         var bookingDetails = {
-            //Id: $scope.bookings.length,
+            Id: $scope.bookings.length,
             PassengerName: $scope.PassengerName,
             PickupLocation: $scope.PickupLocation,
             DropOffLocation: $scope.DropOffLocation,
@@ -17,13 +33,31 @@
             CurrentPassenger: $scope.CurrentPassenger
         };
 
-        $http.post("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking", bookingDetails);
+        $http.push("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking", bookingDetails);
+
+
+        //clears text fields
         $scope.PassengerName = "";
         $scope.PickupLocation = "";
         $scope.DropOffLocation = "";
         $scope.VehicleId = "";
         $scope.CurrentPassenger = "";
+        //need code to reload data
+        
     };
+
+    //function to remove a booking
+    $scope.remove = function (bookingId) {
+        $http.delete("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking/" + bookingId)
+            .success(function (response) {
+                $scope.bookings = response;
+            })
+            .error(function (error) {
+                $scope.errorMessage = error;
+            });
+    }
+
+
 
     // methods for viewing the Bookings
     $scope.viewBookings = false;
@@ -34,6 +68,9 @@
     $scope.closeBookings = function () {
         $scope.viewBookings = false;
     };
+
+
+
     //methods for viewing the add booking form
     $scope.BookingsForm = false;
 
