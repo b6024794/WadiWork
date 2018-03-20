03811d5ff5d5b1e052babf8c6b296bc7da79e5f1
b6024794 <33861818+b6024794@users.noreply.github.com>
2018-03-20 17:04:51 +0000

    Add files via upload

---
+++ TaxiBooking/app/booking/VehicleController.js
@@ -0,0 +1,33 @@
+﻿angular.module("bookingApp").controller("VehicleController", function ($scope, $http) {
+
+    $scope.init = function () {
+        $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/vehicle")
+            .success(function (response) {
+                $scope.Vehicles = response;
+            })
+            .error(function (error) {
+                $scope.errorMessage = error;
+            });
+
+    };
+
+    $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/vehicle")
+        .success(function (response) {
+            $scope.Vehicles = response;
+        })
+        .error(function (error) {
+            $scope.errorMessage = error;
+        });
+
+
+
+    $scope.viewVehicle = true;
+
+    $scope.openVehicle = function () {
+        $scope.viewVehicle = true;
+    };
+    $scope.closeVehicle = function () {
+        $scope.viewVehicle = false;
+    };
+
+});
--- TaxiBooking/app/booking/bookingController.js
+++ TaxiBooking/app/booking/bookingController.js
@@ -11,7 +11,6 @@
 
     };
 
-
     $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking")
         .success(function (response) {
             $scope.bookings = response;
@@ -19,7 +18,6 @@
         .error(function (error) {
             $scope.errorMessage = error;
         });
-
     var bookingId;
 
 
@@ -48,6 +46,7 @@
         $scope.VehicleId = "";
         $scope.CurrentPassenger = "";
         //need code to reload data
+        $scope.init();
     };
 
     //function to remove a booking
@@ -55,11 +54,49 @@
         $http.delete("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking/" + bookingId)
             .success(function (response) {
                 $scope.bookings = response;
+                $scope.init();
             })
             .error(function (error) {
                 $scope.errorMessage = error;
             });
     }
+    //function to edit a response 
+    $scope.displayEditBooking = function (bookingId) {
+        $scope.editBooking = true
+        $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking/" + bookingId)
+            .success(function (response) {
+                $scope.EditPassengerName = response.PassengerName;
+                $scope.EditPickupLocation = response.PickupLocation;
+                $scope.EditDropOffLocation = response.DropOffLocation;
+                $scope.EditVehicleId = response.VehicleId;
+                $scope.EditCurrentPassenger = response.CurrentPassenger;
+                bookingId = response.Id;
+            })
+
+        $scope.edit = function () {
+            var EditDetails = {
+                Id: bookingId,
+                PassengerName: $scope.EditPassengerName,
+                PickupLocation: $scope.EditPickupLocation,
+                DropOffLocation: $scope.EditDropOffLocation,
+                VehicleId: $scope.EditVehicleId,
+                CurrentPassenger: $scope.EditCurrentPassenger
+            };
+
+            $http.put("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking/", EditDetails)
+                .success(function (response) {
+                    $scope.editBooking = false;
+                    $scope.init();
+                })
+        }
+
+
+    }
+
+
+
+
+
 
 
 
@@ -83,5 +120,16 @@
     };
     $scope.closeBookingForm = function () {
         $scope.BookingsForm = false;
+    }
+
+
+    //methods for viewing the edit form
+    $scope.editBooking = false;
+
+    $scope.openEditBooking = function () {
+        $scope.editBooking = true;
+    };
+    $scope.closeEditBooking = function () {
+        $scope.editBooking = false;
     }
 });
