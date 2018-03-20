03778f264e6b35ffafacc89508b162aa9e278a77
b6024794 <33861818+b6024794@users.noreply.github.com>
2018-03-20 17:50:14 +0000

    Add files via upload

--- TaxiBooking/app/booking/VehicleController.js
+++ TaxiBooking/app/booking/VehicleController.js
@@ -10,7 +10,7 @@
             });
 
     };
-
+    //first laoding of vehicles
     $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/vehicle")
         .success(function (response) {
             $scope.Vehicles = response;
@@ -19,9 +19,32 @@
             $scope.errorMessage = error;
         });
 
+    //A function to add a new vehcile
 
+    $scope.addVehicle = function(){
 
-    $scope.viewVehicle = true;
+        var vehicleDetails= {
+            Id: $scope.Vehicles.length,
+            Make: $scope.vehicleMake,
+            Capacity: $scope.vehicleCapacity,
+            Driver: $scope.vehicleDriver,
+            Registration: $scope.vehicleRegistration,
+            Model: $scope.vehicleModel
+        };
+        $http.post("http://webteach_net.hallam.shu.ac.uk/acesjas/api/vehicle", vehicleDetails);
+
+        $scope.vehicleMake = "";
+        $scope.vehicleCapacity = "";
+        $scope.vehicleDriver = "";
+        $scope.vehicleRegistration = "";
+        $scope.vehicleModel = "";
+
+        $scope.init();
+
+    };
+
+    //methods to display vehicle table
+    $scope.viewVehicle = true  ;
 
     $scope.openVehicle = function () {
         $scope.viewVehicle = true;
